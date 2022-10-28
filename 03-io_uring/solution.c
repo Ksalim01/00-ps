ce4105ef13d06cc5b5a05daf86d7a9e76e8aabfe
Salim Kutlush <kutlush.s@phystech.edu>
2022-10-28 18:59:11 +0300

    bugFix

--- 03-io_uring/solution.c
+++ 03-io_uring/solution.c
@@ -8,6 +8,7 @@ typedef char byte;
 const size_t BLOCK_SZ = (size_t)1 << 18;
 const size_t QUEUE_READ_SZ = 4;
 const size_t QUEUE_WRITE_SZ = 4;
+size_t current_queue_size = 0;
 
 struct io_data {
   int read_mode;
@@ -18,7 +19,7 @@ struct io_data {
 
 struct io_data* new_io_data() {
   struct io_data* data = malloc(sizeof(struct io_data));
-  data->buf = malloc(BLOCK_SIZE);
+  data->buf = malloc(BLOCK_SZ);
   return data;
 }
 
@@ -34,6 +35,7 @@ struct fd {
 
 void push_task(struct fd* desc, struct io_uring* ring, struct io_data* data) {
   struct io_uring_sqe* sqe = io_uring_get_sqe(ring);
+
   if (data->read_mode) {
     io_uring_prep_read(sqe, desc->in, data->buf, BLOCK_SZ, data->offset);
   } else {
@@ -42,6 +44,8 @@ void push_task(struct fd* desc, struct io_uring* ring, struct io_data* data) {
   }
   io_uring_sqe_set_data(sqe, data);
   io_uring_submit(ring);
+
+  ++current_queue_size;
 }
 
 void push_read_task(struct fd* desc, struct io_uring* ring) {
@@ -60,10 +64,12 @@ struct io_data* pop_task(struct io_uring* ring) {
   struct io_uring_cqe* cqe;
   io_uring_wait_cqe(ring, &cqe);
   struct io_data* data = io_uring_cqe_get_data(cqe);
-
+  
   data->read_bytes = cqe->res;
 
   io_uring_cqe_seen(ring, cqe);
+
+  --current_queue_size;
   return data;
 }
 
@@ -77,7 +83,7 @@ int copy(int in, int out) {
   push_read_task(&desc, &ring);
   
   int exit_code = 0;
-  for (size_t i = 0; i < QUEUE_READ_SZ + QUEUE_WRITE_SZ; ++i) {
+  while (current_queue_size) {
     struct io_data* data = pop_task(&ring);
     if (data->read_mode && data->read_bytes > 0) {
       data->read_mode = 0;
@@ -87,6 +93,6 @@ int copy(int in, int out) {
       free_io_data(data);
     }
   }
-  
+
   return exit_code;
 }
